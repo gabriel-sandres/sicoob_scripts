e4a945ad9165e0c3ee3e2316e9403ec41901a6cd
Caio Henrique Bemfica Da Rocha <CaioB.Rocha@sicoob.com.br>
2025-07-01 13:45:06 -0300

    Novo PopUp Omni.

---
+++ scriptOmni.js
@@ -0,0 +1,107 @@
+// Função de copiar texto ao clicar no botão "Copiar"
+function copyToClipboard(elementId) {
+    var copyText = document.getElementById(elementId);
+    if (copyText) {
+      copyText.select();
+      document.execCommand("copy");
+      console.log("Copiado pelo botão: " + copyText.value);
+    }
+  }
+  
+  // Modal
+  const modal = document.getElementById("recordingModal");
+  const overlay = document.getElementById("overlay");
+  if (modal && overlay) {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+  } else {
+    console.warn("Modal ou overlay não encontrados.");
+  }
+  
+  function confirmRecording() {
+    document.getElementById("recordingModal").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+  }
+
+  function openRecordingModal() {
+    document.getElementById('recordingModal').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+  }
+  
+  // Função de copiar texto ao clicar no campo
+  function copyOnClick(event) {
+    var copyText = event.target;
+    copyText.select();
+    document.execCommand("copy");
+    console.log("Copiado com clique: " + copyText.value);
+  }
+  
+  const inputs = document.querySelectorAll('input[type="text"]');
+  
+  inputs.forEach(input => {
+    input.addEventListener('click', copyOnClick);
+  });
+  
+  // Função de copiar texto ao focar no campo
+  function copyOnFocus(elementId) {
+    var copyText = document.getElementById(elementId);
+    if (copyText) {
+      copyText.select();
+      document.execCommand("copy");
+      console.log("Copiado com foco: " + copyText.value);
+    }
+  }
+  
+  // Função de preencher o dropdown de "Navegação"
+  function populateDropdown() {
+    var caminhoElement = document.getElementById('Caminho');
+    if (caminhoElement) {
+      var caminho = caminhoElement.value;
+      var caminhoArray = caminho.split(',');
+      var select = document.getElementById('navegacaoURA');
+  
+      select.innerHTML = '';
+  
+      var defaultOption = document.createElement('option');
+      defaultOption.value = "";
+      defaultOption.text = "Navegação:";
+      defaultOption.selected = true;
+      defaultOption.disabled = true;
+      defaultOption.style.color = "#000000";
+      select.appendChild(defaultOption);
+  
+      caminhoArray.forEach(function (item) {
+        var option = document.createElement('option');
+        option.value = item.trim();
+        option.text = item.trim();
+        option.disabled = true;
+        option.style.color = "#000000";
+        select.appendChild(option);
+      });
+    }
+  }
+  
+  window.onload = function () {
+    // Preencher dropdown
+    populateDropdown();
+  }
+  
+  // Exibir skill de entrada
+  const skillOrigemElement = document.getElementById('SkillOrigem');
+  
+  // Botão "Pesquisa"
+  document.getElementById("btnPesquisa").addEventListener("click", function () {
+    this.value = "pesquisa";
+  });
+  
+  document.getElementById('MotivoDesc').addEventListener('change', function () {
+    console.log("Motivo selecionado:", this.value);
+  });
+  
+  // Adicionar as opções ao select
+  optionsToShow.forEach(option => {
+    const opt = document.createElement("option");
+    opt.value = option.value;
+    opt.textContent = option.text;
+    select.appendChild(opt);
+  });
